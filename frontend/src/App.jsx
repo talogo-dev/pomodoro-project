e1474b527df6d522b10172eb85b1c75c9a13b32d
talogo <a27969@alunos.ipca.pt>
2026-01-07 14:13:20 +0000

    feat: initialize frontend project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.jsx) with basic structure for Pomodoro Timer.
    - Added React logo SVG in assets directory.
    - Set up global styles with Tailwind CSS in index.css.
    - Configured main entry point (main.jsx) to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1 class="">Pomodoro Timer</h1>
+      </header>
+      {/* Secção para botoes e afins */}
+      <section>
+        
+      </section>
+      <main>
+        {/* timer */}
+      </main>
+    </>
+  )
+}
+
+export default App
