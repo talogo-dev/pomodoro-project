b3304788e0d8d79b6add55c9a05cb9f5f1da07c3
talogo <a27969@alunos.ipca.pt>
2026-01-09 10:33:52 +0000

    feat: add lucide-react dependency and enhance App component with mode selection style: update index.css with new color variables and font family docs: add dicas.txt for useState usage clarification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [mode, setMode] = useState("focus");
+  const selected = "text-light-green cursor-pointer underline";
+  const notSelected = "text-light-green cursor-pointer";
+
   return (
     <>
-      <header className="flex flex-col mt-5">
-        <h1 className="text-center text-4xl">Pomodoro Timer</h1>
+      <header className="flex flex-col pt-5">
+        <h1 className="text-center text-4xl text-light-green">Pomodoro Timer</h1>
+        <div className="mt-10 flex justify-center gap-10 text-xl">
+          <span className={mode == "focus" ? selected : notSelected} onClick={() => setMode("focus")}>focus</span>
+          <span className={mode == "small-break" ? selected : notSelected} onClick={() => setMode("small-break")}>small break</span>
+          <span className={mode == "long-break" ? selected : notSelected} onClick={() => setMode("long-break")}>long break</span>
+        </div>
       </header>
       {/* Secção para botoes e afins */}
       <section>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <main className="bg-gray-green h-screen">
+      <App />
+    </main>
   </StrictMode>,
 )
